b13afc94e008e3bd76cf17308006a6e99e5efa74
AllenSun101 <allensun21527@gmail.com>
2024-05-19 23:55:22 -0500

    Navbar and footer

---
+++ frontend/app/Footer.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function Footer() {
+    return (
+        <footer className="bg-gray-900">
+            <div className="px-8 py-6 mx-auto">
+                <p className='text-white text-center'> © 2024 ResumAI </p>
+            </div>
+        </footer>
+    )
+}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
       <div>
         <h1>ResumAI</h1>
       </div>
