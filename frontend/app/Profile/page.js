d9d2e2d9dab69d84c267a4797517bc55a19b4a3f
AllenSun101 <allensun21527@gmail.com>
2024-05-20 00:32:07 -0500

    Routing structure

---
+++ frontend/app/Profile/page.js
@@ -0,0 +1,15 @@
+
+export default function Profile(){
+    // fetch from global state
+    return(
+        <div>
+            <p>Profile Information</p>
+            <p>Education Information</p>
+            <p>Professional Experience Information</p>
+            <p>Skills Information</p>
+            <p>Coursework Information</p>
+            <p>Other Categories or Add Custom Information</p>
+
+        </div>
+    )
+}
